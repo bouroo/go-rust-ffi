3894c78048a57fff258eb4c4d976bb8f1df5b0f3
Kawin Viriyaprasopsook <kawin.v@kkumail.com>
2023-10-13 13:48:57 +0700

    📝 godocs

--- lib/rs-qr/src/lib.rs
+++ lib/rs-qr/src/lib.rs
@@ -6,20 +6,28 @@ use std::{fs, ptr, str};
 use fast_qr::convert::{image::ImageBuilder, Builder, Shape};
 use fast_qr::qr::QRBuilder;
 
+// Generate a QR code image and save it to a file
 #[no_mangle]
 pub extern "C" fn gen_qr_img(data: *const libc::c_char, file: *const libc::c_char) {
+    // Convert the input C strings to Rust byte slices
     let data = unsafe { CStr::from_ptr(data).to_bytes() };
     let file = unsafe { CStr::from_ptr(file).to_bytes() };
 
+    // Convert the file byte slice to a PathBuf
     let file_path = PathBuf::from(str::from_utf8(file).unwrap());
+
+    // Get the parent directory of the file path, or use "./tmp" if it doesn't exist
     let parent_dir = file_path.parent().unwrap_or(Path::new("./tmp"));
 
+    // Create the parent directory if it doesn't exist
     fs::create_dir_all(parent_dir).unwrap();
 
+    // Build the QR code using the input data
     let qrcode = QRBuilder::new(str::from_utf8(data).unwrap())
         .build()
         .expect("Failed to build QR code");
 
+    // Create an ImageBuilder with default settings
     let _img = ImageBuilder::default()
         .shape(Shape::RoundedSquare)
         .background_color([255, 255, 255, 191])
@@ -28,20 +36,25 @@ pub extern "C" fn gen_qr_img(data: *const libc::c_char, file: *const libc::c_cha
         .expect("Failed to generate QR code");
 }
 
+// Generate a base64-encoded representation of a QR code image
 #[no_mangle]
 pub extern "C" fn gen_qr_base64(data: *const libc::c_char) -> *mut libc::c_char {
+    // Convert the input C string to a Rust CStr
     let data_cstr = unsafe { CStr::from_ptr(data) };
+
+    // Convert the C string to a UTF-8 string slice
     let data = data_cstr.to_str().expect("Invalid UTF-8 data");
 
-    // Handle potential failure
+    // Build the QR code using the input data and handle potential failure
     let qrcode = match QRBuilder::new(data).build() {
         Ok(qr) => qr,
         Err(e) => {
             eprintln!("Failed to build QR code: {}", e);
-            return std::ptr::null_mut();
+            return ptr::null_mut();
         }
     };
 
+    // Create an ImageBuilder with default settings
     let _img = ImageBuilder::default()
         .shape(Shape::RoundedSquare)
         .background_color([255, 255, 255, 191])
@@ -49,10 +62,13 @@ pub extern "C" fn gen_qr_base64(data: *const libc::c_char) -> *mut libc::c_char 
         .to_bytes(&qrcode)
         .expect("Failed to generate QR code");
 
+    // Encode the image bytes using base64 encoding
     let img_str = format!(
         "data:image/png;base64,{}",
         general_purpose::STANDARD.encode(&_img)
     );
+
+    // Create a CString from the encoded string and handle potential failure
     let cstr_img = match CString::new(img_str) {
         Ok(cstr) => cstr,
         Err(e) => {
@@ -61,20 +77,29 @@ pub extern "C" fn gen_qr_base64(data: *const libc::c_char) -> *mut libc::c_char 
         }
     };
 
+    // Convert the CString into a raw pointer
     cstr_img.into_raw()
 }
 
+// Unit tests
 #[cfg(test)]
 mod tests {
+    // Import the contents of the parent module
     use super::*;
+    // Import the CString type from the std::ffi module
     use std::ffi::CString;
 
+    // Define a unit test function named "test_gen_qr"
     #[test]
     fn test_gen_qr() {
+        // Create a new CString containing the string "Hello, world!"
         let input = CString::new("Hello, world!").unwrap().into_raw();
+        // Create a new CString containing the string "tmp/qr.png"
         let output_file = CString::new("tmp/qr.png").unwrap().into_raw();
 
+        // Call the gen_qr_img function with the input and output_file arguments
         gen_qr_img(input, output_file);
+        // Call the gen_qr_base64 function with the input argument
         gen_qr_base64(input);
     }
 }
--- main.go
+++ main.go
@@ -11,10 +11,6 @@ import (
 )
 
 // main is the entry point of the program.
-//
-// It defines the URL for generating the QR code and the file path for saving the QR code image.
-// It calls the C function gen_qr_img to generate the QR code image and prints the result.
-// It also calls the C function gen_qr_base64 to generate the base64 string of the QR code and prints the result.
 func main() {
 	// Define the URL for generating the QR code
 	qrData := "https://kawin-vir.pages.dev"
@@ -22,11 +18,37 @@ func main() {
 	// Define the file path for saving the QR code image
 	qrFile := "./tmp/qr.png"
 
+	// Generate the QR code image and save it to the file path
+	genQRImg(qrData, qrFile)
+
+	// Generate the base64 string of the QR code
+	result := genQRBase64(qrData)
+
+	// Print the result of generating the QR code image
+	fmt.Printf("genQRBase64(%s): %s %+v\n", qrData, qrFile, result)
+}
+
+// genQRImg generates a QR code image using the provided QR data and saves it to a file.
+//
+// The function takes two parameters:
+// - qrData (string): The data to be encoded in the QR code.
+// - qrFile (string): The path to the file where the QR code image will be saved.
+//
+// The function does not return any value.
+func genQRImg(qrData, qrFile string) {
 	// Call the C function gen_qr_img to generate the QR code image
 	result := C.gen_qr_img(C.CString(qrData), C.CString(qrFile))
 
 	// Print the result of generating the QR code image
-	fmt.Printf("gen_qr_img(%s): %s %+v\n", qrData, qrFile, result)
+	fmt.Printf("genQRImg(%s): %s %+v\n", qrData, qrFile, result)
+
+}
+
+// genQRBase64 generates the base64 string of a QR code using the given qrData.
+//
+// qrData: the data to be encoded in the QR code as a string.
+// goStrValue: the generated base64 string of the QR code as a string.
+func genQRBase64(qrData string) (goStrValue string) {
 
 	// Call the C function gen_qr_base64 to generate the base64 string of the QR code
 	cStrPointer := C.gen_qr_base64(C.CString(qrData))
@@ -35,8 +57,7 @@ func main() {
 	defer C.free(unsafe.Pointer(cStrPointer))
 
 	// Convert the C string to Go string
-	goStrValue := C.GoString(cStrPointer)
+	goStrValue = C.GoString(cStrPointer)
 
-	// Print the result of generating the base64 string of the QR code
-	fmt.Printf("gen_qr_base64(%s): %+s\n", qrData, goStrValue)
+	return
 }
